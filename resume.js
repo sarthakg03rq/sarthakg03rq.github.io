6eee8409b9f0ca0d5d312c3e69d471aa40c33211
G SARTHAK <sarthakg03rq@gmail.com>
2023-09-13 10:28:46 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,1 +1,16 @@
+const container = document.querySelector("#container");
+const logos = document.querySelectorAll("#logo");
+
+logos.forEach((img, index) => {
+  img.style.zIndex = `${logos.length - index}`;
+});
+
+window.addEventListener("mousemove", function (dets) {
+  let x = dets.clientX * 0.05 - 20;
+  let y = dets.clientY * 0.05 - 20;
+
+  logos.forEach((img, index) => {
+    img.style.transform = `translate(${x * index}px, ${y * index}px) `;
+  });
+});
 
